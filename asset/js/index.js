b77e146e3e301b859fe8cc9f0165864d82d3535e
cirejm07 <90808033+cirejm07@users.noreply.github.com>
2022-05-21 06:51:11 +0800

    initial commit

---
+++ asset/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#hamburger1").click(function(){
+        $(this).find("#hamburger2").toggleClass("hide");
+        $("#close").toggleClass("show");
+    }
+    )
+
+    })
