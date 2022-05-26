551a048c52e665ec81220e379347b4f13fcff328
cirejm07 <90808033+cirejm07@users.noreply.github.com>
2022-05-27 05:38:26 +0800

    added developer link scroll animation

--- asset/js/about.js
+++ asset/js/about.js
@@ -5,4 +5,22 @@ $(document).ready(function(){
     }
     )
 
+    
+   
+  
+ 
+
+    var developer = $(".developer")
+    var sun = $(".sun")
+    $(window).scroll(function(){
+        var top = $(this).scrollTop()
+        setTimeout(function(){
+                console.log($(this).scrollTop())
+                developer.css("transform","translateY(" + -top + "px)");
+                sun.css("transform","rotate(" + top + "deg)")
+        }, 100)
+        
+    })
+
+   
 })
